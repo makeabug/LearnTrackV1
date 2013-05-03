acac7084b1eb08d7707d05dc3cb20881f114af0e
Mr.D <makeabug@gmail.com>
2013-05-03 17:43:34 +0800

    footer

--- protected/views/layouts/block.php
+++ protected/views/layouts/block.php
@@ -1,12 +1,12 @@
 <?php $this->beginContent('//layouts/main'); ?>	  
     <div class="row-fluid">
-        <div class="span3">
+        <div class="span2">
              <div class="well sidebar-nav">
                 <?php $this->renderPartial('_sidebar', array('action'=> 
                     !empty($this->actived) ? $this->actived : $this->getAction()->getId())); ?>
              </div>
         </div>
-        <div class="span9">
+        <div class="span10">
             <?php echo $content;?>  
         </div>
     </div>
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -8,39 +8,46 @@
     <style type="text/css" media="all">
         @import url("<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css");
         @import url("<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap-responsive.css");
-        body {
-            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-        }
+        @import url("<?php echo Yii::app()->request->baseUrl; ?>/css/main.css");
     </style>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/jquery.js"></script>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/bootstrap.js"></script>
   </head>
-  <body>	
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-inner">
-            <div class="container">
-                <a class="brand" href="#">LearnTrack</a>
-                <div class="nav-collapse collapse">
-                    <?php 
-                    $this->widget('zii.widgets.CMenu', array(
-                        'id' => 'nav',
-                        'htmlOptions' => array(
-                            'class' => 'nav'
-                        ),
-                        'items' => array(
-                            array('label' => 'Dashboard', 'url'=>array('/site/index')),
-                            //array('label' => 'Blog', 'url'=>array('/post')),
-                        	array('label' => 'Demo', 'url'=>array('/demo')),
-                        ),
-                    ));
-                    ?>
-                </div><!--/.nav-collapse -->
+  <body>
+    <div id="wrap">	
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="#">LearnTrack</a>
+                    <div class="nav-collapse collapse">
+                        <?php 
+                        $this->widget('zii.widgets.CMenu', array(
+                            'id' => 'nav',
+                            'htmlOptions' => array(
+                                'class' => 'nav'
+                            ),
+                            'items' => array(
+                                array('label' => 'Dashboard', 'url'=>array('/site/index')),
+                                //array('label' => 'Blog', 'url'=>array('/post')),
+                            	array('label' => 'Demo', 'url'=>array('/demo')),
+                            ),
+                        ));
+                        ?>
+                    </div><!--/.nav-collapse -->
+                </div>
             </div>
+        </div>
+        
+        <div class="container">
+            <?php echo $content;?>    
         </div>
     </div>
     
-    <div class="container-fluid">
-        <?php echo $content;?>    
+    <!-- footer -->
+    <div id="footer">
+      <div class="container">
+        <p class="muted credit">© 2013 LearnTrack.</p>
+      </div>
     </div>
     
   </body>
