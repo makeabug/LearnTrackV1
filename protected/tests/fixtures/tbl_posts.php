50c92970d91044ddaa183908b80070dd8164e52e
dw@example.com <dengwei@debian.localdomain>
2013-06-18 00:11:39 +0800

    all files

---
+++ protected/tests/fixtures/tbl_posts.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+    'post1' => array(
+        'title' => 'Test Post 1',
+        'content' => 'This is test content 1',
+        'create_time' => '',
+        'create_user_id' => '',
+        'update_time' => '',
+        'update_user_id' => ''
+    ),
+    'post2' => array(
+        'title' => 'Test Post 2',
+        'content' => 'This is test content 2',
+        'create_time' => '',
+        'create_user_id' => '',
+        'update_time' => '',
+        'update_user_id' => ''
+    ),
+    'post3' => array(
+        'title' => 'Test Post 3',
+        'content' => 'This is test content 3',
+        'create_time' => '',
+        'create_user_id' => '',
+        'update_time' => '',
+        'update_user_id' => ''
+    ),
+);
